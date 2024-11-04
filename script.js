658d17b78bad0b28a6e43cb0f457dc7a3b26bea4
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2024-11-04 13:20:32 -0400

    lenis

--- script.js
+++ script.js
@@ -88,11 +88,27 @@
 //     }
 // })
 
-let main = document.getElementById('main');
+// let main = document.getElementById('main');
 
-// Locomotive
-let scroll = new LocomotiveScroll({
-    el: main,
-    smooth: true,
-    lerp: 0.01 
+// // Locomotive
+// let scroll = new LocomotiveScroll({
+//     el: main,
+//     smooth: true,
+//     lerp: 0.01 
+// });
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
 });
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
