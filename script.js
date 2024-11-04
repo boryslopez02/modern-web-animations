7fe8b5bed2cdb7ca7ef5035befe14ddae0c3b230
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2024-11-04 12:58:29 -0400

    locomotive js

--- script.js
+++ script.js
@@ -1,39 +1,98 @@
-gsap.from('#box', {
-    x:300,
-    duration: 1,
-    backgroundColor: 'blue',
-    rotate: 360,
-    borderRadius: '50%',
-    scale: 0.5,
-    delay: 1
-})
+// gsap.from('#box', {
+//     x:300,
+//     duration: 1,
+//     backgroundColor: 'blue',
+//     rotate: 360,
+//     borderRadius: '50%',
+//     scale: 0.5,
+//     delay: 1
+// })
 
-let tl = gsap.timeline();
+// let tl = gsap.timeline();
 
-tl.from('#box1', {
-    x:600,
-    duration: 1,
-    delay: 1
-})
+// tl.from('#box1', {
+//     x:600,
+//     duration: 1,
+//     delay: .1
+// })
 
-tl.from('#box2', {
-    x:500,
-    duration: 1,
-    delay: 1
-})
+// tl.from('#box2', {
+//     x:500,
+//     duration: 1,
+//     delay: .1
+// })
 
-tl.from('#box3', {
-    x:400,
-    duration: 1,
-    delay: 1
-})
+// tl.from('#box3', {
+//     x:400,
+//     duration: 1,
+//     delay: .1
+// })
 
-gsap.from('h1', {
-    y:50,
-    duration: 0.3,
-    scale: 0.5,
-    delay: .6,
-    stagger: 0.5,
-    repeat: -1,
-    yoyo: true
-})
+// gsap.from('h1', {
+//     y:50,
+//     duration: 0.3,
+//     scale: 0.5,
+//     delay: .6,
+//     stagger: 0.5,
+//     repeat: -1,
+//     yoyo: true
+// })
+
+// Scroll Trigger
+
+// gsap.to('#page1 #box1', {
+//     scale: 0,
+//     rotate: 360,
+//     duration: 1,
+//     delay: 1,
+//     scrollTrigger: {
+//         trigger: '#page1 #box1',
+//         scroller: 'body',
+//         markers: true,
+//         start: 'top 60%',
+//         end: 'top 30%',
+//         scrub: 3,
+//         pin: true
+//     }
+// })
+
+// gsap.to('#page2 #box2', {
+//     scale: 0,
+//     rotate: 360,
+//     duration: 1,
+//     delay: 1,
+//     scrollTrigger: {
+//         trigger: '#page2 #box2',
+//         scroller: 'body',
+//         markers: true,
+//         start: 'top 60%',
+//         end: 'top 30%',
+//         scrub: 3,
+//         pin: true
+//     }
+// })
+
+// gsap.to('#page3 #box3', {
+//     scale: 0,
+//     rotate: 360,
+//     duration: 1,
+//     delay: 1,
+//     scrollTrigger: {
+//         trigger: '#page3 #box3',
+//         scroller: 'body',
+//         markers: true,
+//         start: 'top 60%',
+//         end: 'top 30%',
+//         scrub: 3,
+//         pin: true
+//     }
+// })
+
+let main = document.getElementById('main');
+
+// Locomotive
+let scroll = new LocomotiveScroll({
+    el: main,
+    smooth: true,
+    lerp: 0.01 
+});
